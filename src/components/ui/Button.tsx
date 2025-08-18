290fbbef81cd700fe33a5bf715d9b57062308b19
SEO HUI JEONG <pp123qq@dgu.ac.kr>
2025-08-18 16:27:20 +0900

    feat : 버튼 컴포넌트 개발
    - 등록하기/저장하기/삭제하기 버튼 컴포넌트 개발
    - variant 라는 속성(prop)을 통해 버튼의 종류를 지정할 수 있습니다.
    - 관련 예시는 노션 프론트엔드 페이지 참고

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,32 @@
+// Button.tsx
+import React from "react";
+
+interface ButtonProps {
+  variant: "save" | "register" | "delete";
+  onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ variant, onClick }) => {
+  const getText = () => {
+    switch (variant) {
+      case "save":
+        return "저장하기";
+      case "register":
+        return "등록하기";
+      case "delete":
+        return "삭제하기";
+    }
+  };
+
+  return (
+    <button
+      onClick={onClick}
+      className="w-[475px] h-[53px] bg-[#DA5B00] text-white font-bold rounded-md border border-[#ACACAC]"
+    >
+      {getText()}
+    </button>
+  );
+};
+
+export default Button;
+
